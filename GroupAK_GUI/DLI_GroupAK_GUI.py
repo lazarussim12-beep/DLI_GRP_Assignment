872cdb3ac76447172e2547a9d9a32b7711c90465
Lazarus Sim (TP069022) <lazarussim12@gmail.com>
2025-08-23 18:50:22 +0800

    Update DLI_GroupAK_GUI.py

--- GroupAK_GUI/DLI_GroupAK_GUI.py
+++ GroupAK_GUI/DLI_GroupAK_GUI.py
@@ -3,7 +3,7 @@ import re
 import pickle
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer  
+from tensorflow import keras  
 from tensorflow.keras.models import load_model
 import xgboost as xgb
 import lightgbm as lgb
@@ -325,4 +325,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
